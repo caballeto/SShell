c3011d7a4eeb1f630c1cbf8582a7564ff23e1509
caballeto <elminsteraumar4@gmail.com>
2019-02-04 18:10:44 +0200

    Readme.

--- README.md
+++ README.md
@@ -1,1 +1,26 @@
 # SShell
+
+SShell (Simple Shell) is an implementation of simple unix-like shell using 
+C/C++ APIs.
+
+## Commands
+
+Implemented commands are presented below. If command is unknown 
+application will try to execute it in default bash.
+
+- `cd` - change directory
+- `pwd` - get current directory
+- `cat` - print contents of file
+- `touch` - create empty file
+- `rm` - delete file
+- `ls` - print files in current directory
+- `help` - help
+- `exit` - end session
+
+## Run
+
+Run with command:
+
+```
+./bin/main 
+```
---
+++ cmake-build-debug/CMakeFiles/Shell.dir/DependInfo.cmake
@@ -0,0 +1,22 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/caballeto/CLionProjects/Shell/commands.cpp" "/home/caballeto/CLionProjects/Shell/cmake-build-debug/CMakeFiles/Shell.dir/commands.cpp.o"
+  "/home/caballeto/CLionProjects/Shell/main.cpp" "/home/caballeto/CLionProjects/Shell/cmake-build-debug/CMakeFiles/Shell.dir/main.cpp.o"
+  "/home/caballeto/CLionProjects/Shell/utilities.cpp" "/home/caballeto/CLionProjects/Shell/cmake-build-debug/CMakeFiles/Shell.dir/utilities.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
